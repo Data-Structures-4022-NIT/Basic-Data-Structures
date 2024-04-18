ec64f8d4f501c79c119a4de722d529cd295ea827
kosar <salarikosar5@gmail.com>
2024-04-18 16:48:58 +0330

    All Function completed

---
+++ Answers/Maximum Subarray Sum.py
@@ -0,0 +1,16 @@
+def MaximumSubarraySum(Array):
+    n=len(Array)
+    Max=Array[0]
+    for i in range(0,n):
+        j=i+1
+        s=Array[i]
+        while(j < n):
+            if ( s >= Max ):
+                Max=s
+            s = s + Array[j]
+            j=j+1
+
+    return Max
+
+array = [-2, -1, -3, 4, -1, 2, 1, -5, 4]
+print(MaximumSubarraySum(array))
