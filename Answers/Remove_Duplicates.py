2938c2653cd2477f1f895bdbab9788f708cef652
kosar <salarikosar5@gmail.com>
2024-04-18 15:30:45 +0330

    complete RemoveDuplicates function

---
+++ Answers/Remove_Duplicates.py
@@ -0,0 +1,9 @@
+def RemoveDuplicates(Array):
+    lenOfArray=0
+    for i in range(0 , len(Array)):
+        if(Array[i]!= Array[i-1]):
+            lenOfArray = lenOfArray+1
+    return lenOfArray         
+
+array = [1 , 1, 2 , 2 , 3, 4 , 5, 5]
+print(RemoveDuplicates(array))
